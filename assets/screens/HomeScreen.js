33cd9b920abaeb45f8c1489e27b58f9402a97f26
Sp0k <contact@gabsavard.com>
2024-05-28 07:17:14 -0300

    style: update the header
    - Updated the look of the header and added the necessary components.
    - Created a header component to take care of the home screen header.
    
    Signed-off-by: Sp0k <contact@gabsavard.com>

--- assets/screens/HomeScreen.js
+++ assets/screens/HomeScreen.js
@@ -1,38 +1,54 @@
-import { Text, SafeAreaView, TouchableOpacity } from "react-native";
-import { useEffect, useState, useLayoutEffect } from "react";
+import { Text, TouchableOpacity, SafeAreaView } from "react-native";
+import { useEffect, useState } from "react";
 import MasonryList from "@react-native-seoul/masonry-list";
-import tw, { useDeviceContext } from "twrnc";
+import tw from "twrnc";
+// import {
+//   useSearchNotesQuery,
+//   useAddNoteMutation,
+//   useDeleteNoteMutation,
+// } from "./db";
 
-import Card from "../components/Card";
 import FillList from "../components/FillList";
+import Card from "../components/Card";
 
 function HomeScreen({ navigation }) {
-  useDeviceContext(tw);
+  // const { data: searchData, error, isLoading } = useSearchNotesQuery("");
+  // const [addNote, { data: addNoteData, error: addNoteError }] =
+  //   useAddNoteMutation();
+  // const [deleteNode] = useDeleteNoteMutation();
+  //
+  // useEffect(() => {
+  //   if (addNoteData != undefined) {
+  //     console.log(addNoteData.title);
+  //     navigation.navigate("Edit", { data: addNoteData });
+  //   }
+  // }, [addNoteData]);
+  const data = FillList(20);
 
-  const data = FillList();
-
-  const onPressHandler = () => {
+  const cardOnPressHandler = () => {
     navigation.navigate("Note");
   };
 
-  const renderItem = ({ item, i }) => (
-    <Card onPress={onPressHandler} item={item} />
+  const renderItem = ({ item }) => (
+    <Card item={item} onPress={cardOnPressHandler} />
   );
-
-  useLayoutEffect(() => {
-    navigation.setOptions({});
-  }, [navigation]);
 
   return (
     <SafeAreaView style={tw`w-full h-full`}>
       <MasonryList
+        style={tw`px-0.5 pt-0.5 pb-20`}
         data={data}
-        keyExtractor={(item) => item.id}
-        numColums={3}
-        showsVerticalScrollIndicator={false}
+        numColumns={2}
         renderItem={renderItem}
-        onEndReachedThreshold={0.1}
+        keyExtractor={(item) => item.id}
+        showsVerticalScrollIndicator={false}
       />
+      <TouchableOpacity
+        onPress={() => console.log("New item")}
+        style={tw`bg-blue-500 rounded-full absolute bottom-[8%] right-10 mx-auto items-center flex-1 justify-center w-14 h-14`}
+      >
+        <Text style={tw`text-white text-center text-3xl mt--1`}>+</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
