fbbf67a6da425982c88ef8b23460b9b1a11fc4b3
Sp0k <contact@gabsavard.com>
2024-06-01 23:17:38 -0300

    chore(comments): Commented all of the components
    - Finished commenting all of the components file
    
    Signed-off-by: Sp0k <contact@gabsavard.com>

--- assets/components/Header.js
+++ assets/components/Header.js
@@ -1,12 +1,24 @@
+// External imports
 import { SafeAreaView, Text, TextInput, Platform } from "react-native";
 import tw from "twrnc";
 
+/* Header
+ * The custom component for the header of the home screen. It displays
+ * the word 'notes' and a search bar to search through notes.
+ *
+ * @params: searchInput: Pointer to the state variable of the search data
+ *          setSearchInput: Function to use the search feature from the
+ *                          database
+ * @return: Custom header component used in the home screen
+ */
 const Header = ({ searchInput, setSearchInput }) => {
+  // Style for the size of the header based on the platform used
   const twStyle_view =
     Platform.OS === "ios"
       ? tw`bg-[#030712] h-39`
       : tw`bg-[#030712] h-auto py-2.5`;
 
+  // Style for the size of the search bar based on the platform used
   const twStyle_input =
     Platform.OS === "ios"
       ? tw`text-white bg-gray-800 w-[96%] h-11 mx-auto rounded-lg px-2`
--- assets/components/ShareButton.js
+++ assets/components/ShareButton.js
@@ -1,7 +1,17 @@
+// External imports
 import { TouchableOpacity, Text, Share } from "react-native";
 import tw from "twrnc";
 
+/* Share Button
+ * A custom component for the button used in the note editing screen in
+ * order to share the note.
+ *
+ * @params: title: The note's title data
+ *          content: The note's content data
+ * @return: A custom button that opens the share system
+ */
 function ShareButton({ title, content }) {
+  // Share function handler for when the button is pressed
   const shareHandler = async () => {
     Share.share(
       {
