4dca08ad9168d1eb8a2649ce4ab6503427e40b1e
Sp0k <contact@gabsavard.com>
2024-06-01 23:58:22 -0300

    chore(comments): finish commenting the files
    - Commented the screens
    
    Signed-off-by: Sp0k <contact@gabsavard.com>

--- assets/screens/HomeScreen.js
+++ assets/screens/HomeScreen.js
@@ -9,23 +9,24 @@ import {
 import { useEffect, useState, useLayoutEffect } from "react";
 import MasonryList from "@react-native-seoul/masonry-list";
 import tw from "twrnc";
-import { useSearchNotesQuery, useAddNoteMutation } from "../../db";
 
 // Internal Imports
+import { useSearchNotesQuery, useAddNoteMutation } from "../../db";
 import Card from "../components/Card";
 import AddButton from "../components/AddButton";
 import Header from "../components/Header";
 
+/* Home Screen
+ * The home screen component. It has a header which can be used to
+ * search for notes. It also displays all of the notes (or the ones
+ * that are left when using the search feature) using a MasonryList.
+ *
+ * @params: navigation: The app's navigation
+ * @return: The home screen displaying the notes and the header.
+ */
 function HomeScreen({ navigation }) {
-  const cardOnPressHandler = () => {
-    navigation.navigate("Note");
-  };
-  const renderItem = ({ item }) => (
-    <Card
-      item={item}
-      onPress={() => navigation.navigate("Note", { data: item })}
-    />
-  );
+  // Component's constants
+  // Search input and data function and states
   const [searchInput, setSearchInput] = useState("");
   const {
     data: searchData,
@@ -33,11 +34,26 @@ function HomeScreen({ navigation }) {
     isLoading,
   } = useSearchNotesQuery(searchInput);
 
+  // Handler for the pressing the notes' cards
+  const cardOnPressHandler = () => {
+    navigation.navigate("Note");
+  };
+
+  // Turning notes into cards
+  const renderItem = ({ item }) => (
+    <Card
+      item={item}
+      onPress={() => navigation.navigate("Note", { data: item })}
+    />
+  );
+
+  // The width of the list based on the platform used
   const twStyle_list =
     Platform.OS === "ios"
       ? tw`px-0.5 pt-0.5 pb-20`
       : tw`w-[95%] pt-0.5 pb-20 mx-auto`;
 
+  // Updating the header when the screen is loaded
   useLayoutEffect(() => {
     navigation.setOptions({
       header: () => (
--- assets/screens/NoteScreen.js
+++ assets/screens/NoteScreen.js
@@ -1,3 +1,4 @@
+// External imports
 import {
   TextInput,
   View,
@@ -10,29 +11,44 @@ import {
 } from "react-native";
 import { useState, useEffect, useLayoutEffect } from "react";
 import tw, { useDeviceContext } from "twrnc";
-import { useUpdateNoteMutation, useDeleteNoteMutation } from "../../db";
 
+// Internal imports
+import { useUpdateNoteMutation, useDeleteNoteMutation } from "../../db";
 import DeleteButton from "../components/DeleteButton";
 import ShareButton from "../components/ShareButton";
 
+/* Note (editing) Screen
+ * The screen on which notes will be edited, deleted and shared. It's
+ * header holds the share and delete button. The rest of it's screen
+ * holds a TextInput for the title and another for the content.
+ *
+ * @params: route: The navigation route used to get to the screen
+ *          navigation: The app's navigation
+ * @returns: The screen used to edit notes holding two TextInputs
+ */
 function NoteScreen({ route, navigation }) {
   useDeviceContext(tw);
 
-  const data = route.params.data;
-  const [updateNote] = useUpdateNoteMutation();
-  const [title, setTitle] = useState(data.title);
-  const [content, setContent] = useState(data.content);
-  const [deleteNote] = useDeleteNoteMutation();
-  const [cursorPosition, setCursorPosition] = useState({ end: 0, start: 0 });
+  // Component's constants
+  const data = route.params.data; // The note's data
+  const [updateNote] = useUpdateNoteMutation(); // Function to update the db
+  const [title, setTitle] = useState(data.title); // State of the title
+  const [content, setContent] = useState(data.content); // State of the content
+  const [deleteNote] = useDeleteNoteMutation(); // Function to delete notes from db
+  const [cursorPosition, setCursorPosition] = useState({ end: 0, start: 0 }); // State to keep track of cursor position on the screen
 
+  // The style of the title area based on the platform used
   const twStyle_title =
     Platform.OS === "ios"
       ? tw`text-xl mb-2 mt-1 text-white border-b-2 border-gray-700 pb-1`
       : tw`text-xl mt-1 text-white border-b-2 border-gray-700 pb-1`;
 
+  // Variables used to verify if notes are empty
   let emptyContent = !content ? true : false;
   let emptyTitle = !title ? true : false;
 
+  // Update the header when the screen is loaded as well as when the
+  // data (title or content) is updated
   useLayoutEffect(() => {
     navigation.setOptions({
       headerTitle: "",
@@ -45,6 +61,7 @@ function NoteScreen({ route, navigation }) {
     });
   }, [title, content]);
 
+  // Checks if the note is empty, deletes it when leaving the screen if it is
   useEffect(() => {
     return () => {
       if (emptyContent && emptyTitle) {
